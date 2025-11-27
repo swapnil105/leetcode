c824cc9cd057b0f28d86d1e24262ec4111cc6d18
SwapNil <117698918+swapnil105@users.noreply.github.com>
2025-11-27 20:45:32 +0530

    Time: 0 ms (100%), Space: 12.8 MB (98.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        hash_map = {}  # value : index
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return [hash_map[complement], i]
+            hash_map[num] = i
